ab6218aa954b764ac0d0881581d95a5dbfeef407
Rhearepe18 <54646480+Rhearepe18@users.noreply.github.com>
2019-10-15 20:26:56 -0400

    Add files via upload

---
+++ FE 595 Hello.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello_world():
+    print("what should I print?")
+    return "Hello, world!!!"
+
+
+@app.route('/hello/<name>', methods=['GET'])
+def hello_person(name):
+    return render_template('hello.html', name=name)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
+
